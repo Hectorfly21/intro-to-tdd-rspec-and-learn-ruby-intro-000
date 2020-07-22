5d6e3197b1d1a278f2186ad5dd0ce1be05fa423d
Hector <hectorfly21@gmail.com>
2020-07-22 01:11:45 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -7,6 +7,6 @@ describe 'current_age_for_birth_year method' do
     birth_year = 1984
     answer = 19
     age_of_person = current_year
-    expect(age_of_person).to eq(19)
+    expect(age_of_person).to eq(answer)
  end
 end
